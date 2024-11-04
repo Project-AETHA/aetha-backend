cfe06b3b1414e8797e6ce993e5fd7c2a4f3f5e8b
NisalWishwajithPeiris <2021is073@stu.ucsc.cmb.ac.lk>
2024-11-04 16:39:30 +0530

    add @debRef part instead of document reference

--- src/main/java/com/nighthawk/aetha_backend/entity/Comment.java
+++ src/main/java/com/nighthawk/aetha_backend/entity/Comment.java
@@ -1,13 +1,12 @@
 package com.nighthawk.aetha_backend.entity;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
-
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 @Data
@@ -18,9 +17,12 @@ public class Comment {
         @Id
         private String id;
         private String content;
-        @DocumentReference(collection = "novels")
+
+        @DBRef(lazy = true)
         private Novel novel;
-        @DocumentReference(collection = "users")
+
+        @DBRef(lazy = true)
         private AuthUser user;
-        private Date createdAt;
+
+        private LocalDate createdAt = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 }
